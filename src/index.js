b49b79eb848a5888134a96023d8f60166f4b4ff3
Aruna Turay <tura_arun@yahoo.com>
2023-05-13 16:29:57 -0400

    create card

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 // write your code here
+console.log("js loading...")
+
+const collectMenuData =  () => {
+  return fetch("http://localhost:3000/ramens") 
+    .then(response => (response.json)())
+    .then(ramenInfo => ramenInfo.forEach((ramen) => createCard(ramen)));
+
+ }
+ collectMenuData()
+
+ function createCard() {
+  let displayDiv =  document.getElementById("ramen-menu")
+
+  let img = document.createElement("img")
+  img.src = ramen.image
+
+  displayDiv.appendChild(img)
+
+
+ }
