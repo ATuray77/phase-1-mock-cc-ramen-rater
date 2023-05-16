9791db6e6e1a74aa4f2100f90963a421ebb2f6f8
Aruna Turay <tura_arun@yahoo.com>
2023-05-16 01:58:44 -0400

    event listener

--- src/index.js
+++ src/index.js
@@ -9,13 +9,39 @@ const collectMenuData =  () => {
  }
  collectMenuData()
 
- function createCard() {
-  let displayDiv =  document.getElementById("ramen-menu")
-
-  let img = document.createElement("img")
+ function createCard(ramen) {
+  const displayDiv =  document.getElementById("ramen-menu")
+  const img = document.createElement("img")
   img.src = ramen.image
-
   displayDiv.appendChild(img)
 
 
+  img.addEventListener('click', (e) => {
+  e.target.img
+  console.log(img)
+    
+    const imgDetail =document.getElementsByClassName("img#detail-image")
+    imgDetail.src = ramen.image  
+
+    const h2 = document.querySelector("h2")
+    h2.textContent= ramen.name
+  
+    const h3Restaurant = document.querySelector(".restaurant")
+    h3Restaurant.textContent = ramen.restaurant
+   
+
+    const span = document.querySelector("span#rating-display")
+    span.textContent = ramen.rating
+    const p = document.querySelector("p")
+    p.appendChild(span)
+  
+    const pComment = document.querySelector("p#comment-display")
+    pComment.textContent = ramen.comment
+  
+    ramen-detail.apppend(img, imgDetail, h2, h3, h3Restaurant, span, pComment )
+  
+  })
+  
+
  }
+
